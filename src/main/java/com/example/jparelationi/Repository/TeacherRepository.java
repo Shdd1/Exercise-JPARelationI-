013b53df95c72c2260ec02ea4cd5b51509dbda64
Shahad Alhazmi <129505192+Shdd1@users.noreply.github.com>
2024-08-21 17:59:40 +0300

    Add files via upload

---
+++ src/main/java/com/example/jparelationi/Repository/TeacherRepository.java
@@ -0,0 +1,12 @@
+package com.example.jparelationi.Repository;
+
+import com.example.jparelationi.Model.Teacher;
+import jakarta.persistence.Id;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
+    Teacher findTeacherById(Integer id);
+
+}
